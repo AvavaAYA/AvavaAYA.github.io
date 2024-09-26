485c2ac8ebc77ae549bd42b88adc1100630a98d1
avavaaya <nana7777777mi@gmail.com>
2024-09-26 13:47:03 +0800

    Quartz sync: Sep 26, 2024, 1:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -26,28 +26,53 @@ const config: QuartzConfig = {
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
+
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          light: "#fbf1c7", // 背景色（浅）
+          lightgray: "#ebdbb2", // 浅灰色
+          gray: "#d5c4a1", // 灰色
+          darkgray: "#a89984", // 深灰色
+          dark: "#3c3836", // 文字颜色（深）
+          secondary: "#458588", // 次要颜色（蓝色调）
+          tertiary: "#689d6a", // 第三颜色（青色调）
+          highlight: "rgba(215, 153, 33, 0.15)", // 高亮颜色（带透明度的黄色）
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          light: "#282828", // 背景色（深）
+          lightgray: "#a89984", // 浅灰色（边框 / 高亮）
+          gray: "#928374", // 灰色
+          darkgray: "#fbf1c7", // 正文文字
+          dark: "#ebdbb2", // 文字颜色（浅）
+          secondary: "#8ec07c", // 次要颜色（蓝色调）
+          tertiary: "#83a598", // 第三颜色（青色调）
+          highlight: "rgba(215, 153, 33, 0.15)", // 高亮颜色（带透明度的黄色）
         },
       },
+
+      // colors: {
+      //   lightMode: {
+      //     light: "#faf8f8",
+      //     lightgray: "#e5e5e5",
+      //     gray: "#b8b8b8",
+      //     darkgray: "#4e4e4e",
+      //     dark: "#2b2b2b",
+      //     secondary: "#284b63",
+      //     tertiary: "#84a59d",
+      //     highlight: "rgba(143, 159, 169, 0.15)",
+      //   },
+      //   darkMode: {
+      //     light: "#161618",
+      //     lightgray: "#393639",
+      //     gray: "#646464",
+      //     darkgray: "#d4d4d4",
+      //     dark: "#ebebec",
+      //     secondary: "#7b97aa",
+      //     tertiary: "#84a59d",
+      //     highlight: "rgba(143, 159, 169, 0.15)",
+      //   },
+      // },
+
     },
   },
   plugins: {
